6015bea7be3304689b6281294d9e62defae5bef0
Dima121880 <surguladze.dima33@gmail.com>
2024-11-12 00:30:28 +0400

    add js teg

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let name = "Dima";
+let dayOfWeek = 10.12;
+let favoriteActivity = "dance";
+
+console.log(name, dayOfWeek, favoriteActivity);
+
+const username = "Hi, my name is ";
+const dayof = "Today is ";
+const activi = " a perfect day for";
+console.log(username + name, dayof + dayOfWeek, activi + favoriteActivity);
