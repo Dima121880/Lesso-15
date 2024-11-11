d5d808b2046f02d289c84f6738c40346c19232f7
Dima121880 <surguladze.dima33@gmail.com>
2024-11-12 00:36:00 +0400

    add js consol

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ const username = "Hi, my name is ";
 const dayof = "Today is ";
 const activi = " a perfect day for";
 console.log(username + name, dayof + dayOfWeek, activi + favoriteActivity);
+
+const numb1 = username + name;
+console.log(numb1);
+const numb2 = dayof + dayOfWeek;
+console.log(numb2);
+const numb3 = activi + favoriteActivity;
+console.log(numb3);
